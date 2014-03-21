e08dbb1448b786383d7fea55b835be6b1f2aec64
NellielX <gae.marzilli@gmail.com>
2014-03-21 11:36:43 +0100

    Update design BackOffice

--- backoffice.php
+++ backoffice.php
@@ -12,44 +12,43 @@ require_once 'fragments/traitements/inscrit.php';
     #text_pub{
         display: none;
     }
-    table, td, th {
-        border: 1px solid black
-    }
-    table{
-        margin: auto;
-    }
 </style>
-<table>
-<?php
-$inscrits = showUsers();
-    foreach ($inscrits as $inscrit) {
-?>
-    <tr>
-        <th>ID</th>
-        <th>Nom</th>
-        <th>Prénom</th>
-        <th>Login</th>
-        <th>MDP</th>
-        <th>Email</th>
-        <th>Etat</th>
-        <th>Action</th>
-    </tr>
-    <tr>
-		<td><?=$inscrit['num_inscrit'] ?></td>
-		<td><?=$inscrit['nom_inscrit'] ?></td>
-		<td><?=$inscrit['prenom_inscrit'] ?></td>
-		<td><?=$inscrit['nom_compte_inscrit'] ?></td>
-		<td><?=$inscrit['mdp_inscrit'] ?></td>
-		<td><?=$inscrit['adresse_mail_inscrit'] ?></td>
-		<td><?=$inscrit['etat_inscrit'] ?></td>
-                <td><a href="fragments/traitements/bo_suppr_inscrit.php?id=<?php echo $inscrit['num_inscrit'] ?>">Supprimer</a></td>
-    </tr>
 
+<div id="conditions">
+    <div id="text_conditions">
+			<table>
+			    <tr>
+			        <th>ID</th>
+			        <th>Nom</th>
+			        <th>Prénom</th>
+			        <th>Login</th>
+			        <th>MDP</th>
+			        <th>Email</th>
+			        <th>Etat</th>
+			        <th>Action</th>
+			    </tr>
+			<?php
+			$inscrits = showUsers();
+			    foreach ($inscrits as $inscrit) {
+			?>
+			    <tr>
+				<td><?=$inscrit['num_inscrit'] ?></td>
+				<td><?=$inscrit['nom_inscrit'] ?></td>
+				<td><?=$inscrit['prenom_inscrit'] ?></td>
+				<td><?=$inscrit['nom_compte_inscrit'] ?></td>
+				<td><?=$inscrit['mdp_inscrit'] ?></td>
+				<td><?=$inscrit['adresse_mail_inscrit'] ?></td>
+				<td><?=$inscrit['etat_inscrit'] ?></td>
+				<td><a href="fragments/traitements/bo_suppr_inscrit.php?id=<?php echo $inscrit['num_inscrit'] ?>">Supprimer</a></td>
+			    </tr>
+			    
+			<?php
+			    }
+			?>
+			</table>
+	</div>
+</div>
 
-<?php
-    }
-?>
-</table>
 <?php
 include ($_SERVER['DOCUMENT_ROOT'].'/VieDeGeek/fragments/shared/footer.php');
 ?>
